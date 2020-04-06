48aade13b5b74ac47fa7830b6c0d09c5298b13c5
christian <christianipc.1987@gmail.com>
2020-04-05 21:22:47 -0300

    01_consultas, test de api creo una vista que retorne informacion en formato json, solo deje el modelo de Integrante, investigar si la carpeta migrations es necesario subir al repositorio

--- 01_consultas/consultas_con_django/fixture/admin.py
+++ 01_consultas/consultas_con_django/fixture/admin.py
@@ -3,10 +3,7 @@ from .models import *
 # Register your models here.
 
 
-admin.site.register(Integrante)
-admin.site.register(Jugador)
-admin.site.register(Nacionalidad)
-admin.site.register(DT)
-admin.site.register(Provincia)
-admin.site.register(Localidad)
-admin.site.regiter(Partido)
+class IntegranteAdmin(admin.ModelAdmin):
+    list_display = ('nombre','fecha_nacimiento')
+
+admin.site.register(Integrante, IntegranteAdmin)
--- 01_consultas/consultas_con_django/fixture/views.py
+++ 01_consultas/consultas_con_django/fixture/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from fixture.models import Integrante
+
 
 # Create your views here.
+def getIntegrantes(request):
+    registros = Integrante.objects.all()
+    respuesta = {
+        "type":"Integrante",
+        "features":[]
+    }
+
+    for registro in registros:
+        dato = {
+            "cod_integrante": registro.cod_integrante,
+            "nombre": registro.nombre,
+            "fecha_nacimiento": registro.fecha_nacimiento        
+        }
+        respuesta["features"].append(dato)
+    # test = {
+    #         "cod_integrante": '001',
+    #         "nombre": 'teodorico',
+    #         "fecha_nacimiento": '08-10-1957',
+    #         "telefonos":[
+    #             {
+    #                 "telefono": "46446645"
+    #             },
+    #             {
+    #                 "telefono": "46690289"   
+    #             }
+    #         ]        
+    #     }
+    return JsonResponse(respuesta)
